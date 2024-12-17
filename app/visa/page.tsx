7c81464a5ded061453d782d3a7ebad213023db00
Zixuan Wang <zx_will@outlook.com>
2024-12-17 23:34:28 +0000

    ready for Content Box dev

--- app/visa/page.tsx
+++ app/visa/page.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import InputPanel from "@/components/InputPanel";
-
+import ContentBox from "@/components/ContentBox";
 const page = () => {
   return (
     <>
-      <div className="flex flex-1 px-8 pb-5 w-full max-h-[calc(100vh-5rem)]">
+      <div className="flex flex-1 px-8 pb-5 w-full max-h-[calc(100vh-5rem)] gap-4">
         <InputPanel />
-        <div className="bg-gray-200 rounded-lg w-full h-full"></div>
+        <ContentBox />
       </div>
     </>
   );
---
+++ components/ContentBox.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ContentBox = () => {
+  return <div className="bg-gray-300 rounded-[2rem] w-full h-full"></div>;
+};
+
+export default ContentBox;
