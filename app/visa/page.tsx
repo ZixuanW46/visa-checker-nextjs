86000a5b5f75a2f3abfeab65b1700b2c43de06e8
Zixuan Wang <zx_will@outlook.com>
2024-12-26 23:35:09 +0000

    suspense wrapper added

--- app/visa/page.tsx
+++ app/visa/page.tsx
@@ -1,15 +1,20 @@
-import React from "react";
+import React, { Suspense } from "react";
 import InputPanel from "@/components/InputPanel";
 import ContentBox from "@/components/ContentBox";
-const page = () => {
+
+const Page = () => {
   return (
     <>
       <div className="flex flex-1 px-8 pb-5 w-full max-h-[calc(100vh-5rem)] gap-4">
-        <InputPanel />
-        <ContentBox />
+        <Suspense fallback={<div>Loading input panel...</div>}>
+          <InputPanel />
+        </Suspense>
+        <Suspense fallback={<div>Loading content...</div>}>
+          <ContentBox />
+        </Suspense>
       </div>
     </>
   );
 };
 
-export default page;
+export default Page;
